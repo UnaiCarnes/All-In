afec296aef605ff1fb1a12cc2e267e9e632c32ce
UnaiCarnes <ucarnesat23dw@ikzubirimanteo.com>
2024-11-28 08:03:59 +0100

    Most components in use and fixed

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import Card from '../components/ui/Card';
 
 const Games = () => {
   return (
     <div 
-      className="min-h-screen"
+      className="h-[calc(100vh-6rem)] overflow-hidden"  
       style={{
         backgroundImage: "url('/img/fondo1.webp')",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
       }}
     >
-      <Card className="mb-6">
-        <h1 className="text-2xl font-bold text-yellow-500 mb-4">
-          Available Games
-        </h1>
-        <p className="text-gray-300">
-          Select a game from the sidebar to start playing!
-        </p>
-      </Card>
-
-      {/* Aquí puedes agregar más contenido específico de juegos */}
+      {/* Aquí se añadirán los juegos en el futuro */}
     </div>
   );
 };
